88b1c6e6731440df621be3bcc38bd1eee4ca11b1
Celeste Lavoie <celeste.lavoie@gmail.com>
2016-04-22 10:33:57 -0400

    added classes, fixed js and query links, created var, added waypoint js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var $brain = $('.brain-box')
+var $volunteer = $('.volunteer')
+var $plan = $('.plan')
+var $argue = $('.argue')
+var $factual = $('.factual')
+var $alone = $('.alone')
+var $avoid = $('.avoid')
+var $priority = $('.prioritize')
+var $interest = $('.interest')
+var $track = $('.track')
+var $possible = $('.possibilities')
+var $organize = $('.organize')
+var $lecture = $('.lecture')
+
+$brain{
+
+}
+
+$volunteer
