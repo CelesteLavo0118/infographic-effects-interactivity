ed7044bee92c99ea40840178ff4453477bd29a86
Celeste Lavoie <celeste.lavoie@gmail.com>
2016-04-29 08:41:36 -0400

    committed before saved

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-var $brain = $('.brain-box')
-var $volunteer = $('.volunteer')
-var $plan = $('.plan')
-var $argue = $('.argue')
-var $factual = $('.factual')
-var $alone = $('.alone')
-var $avoid = $('.avoid')
-var $priority = $('.prioritize')
-var $interest = $('.interest')
-var $track = $('.track')
-var $possible = $('.possibilities')
-var $organize = $('.organize')
-var $lecture = $('.lecture')
+var $brain = $('.brain-box');
+var $volunteer = $('.volunteer');
+var $plan = $('.plan');
+var $argue = $('.argue');
+var $factual = $('.factual');
+var $alone = $('.alone');
+var $avoid = $('.avoid');
+var $priority = $('.prioritize');
+var $interest = $('.interest');
+var $track = $('.track');
+var $possible = $('.possibilities');
+var $organize = $('.organize');
+var $lecture = $('.lecture');
 
 $brain.waypoint(function(direction) {
   if (direction == 'down') {
